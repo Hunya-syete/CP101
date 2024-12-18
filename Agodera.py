8fbfcb8fac2b6ac4351d02c1cd3a2180cccc966e
Agodera <eurekamarie377@gmail.com>
2024-12-18 19:44:36 +0800

    Create Agodera.py

---
+++ Agodera.py
@@ -0,0 +1,8 @@
+number = float(input("Enter a number of your choice: "))
+
+if number > 0:
+    print ("the number is positive!")
+elif number == 0:
+    print ("the number is equal to zero!")
+else:
+    print ("the number is negative!")
